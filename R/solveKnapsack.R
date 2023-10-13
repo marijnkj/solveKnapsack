9a39cf9a891eb4f13004c6f6867554c47d18cac4
Marijn <marijnjaarsma@factsconsultancy.nl>
2023-10-13 11:01:55 +0200

    Brute force function works

--- R/solveKnapsack.R
+++ R/solveKnapsack.R
@@ -1,3 +1,5 @@
+library(stats)
+
 RNGversion(min(as.character(getRversion()),"3.5.3"))
 
 ##old sampler used for backward compatibility
@@ -15,7 +17,22 @@ brute_force_knapsack <- function(x, W) {
     stop("Check your variables! x must be a data.frame with columns w and v and all positive values, and W must be a scalar value.")
   }
   
+  max_value <- 0
+  elements <- vector()
   
+  for (i in (1:2^nrow(x) - 1)) {
+    combination <- intToBits(i)
+    x_comb <- x[combination == 1,]
+    value <- sum(x_comb$v)
+    weight <- sum(x_comb$w)
+    
+    if (weight <= W & value > max_value) {
+      max_value <- value
+      elements <- rownames(x_comb)
+    }
+  }
+  
+  return(list(value=max_value, elements=elements))
 }
 
 dynamic_knapsack <- function(x, W) {
@@ -23,6 +40,19 @@ dynamic_knapsack <- function(x, W) {
     stop("Check your variables! x must be a data.frame with columns w and v and all positive values, and W must be a scalar value.")
   }
   
+  mat <- matrix(0, nrow=nrow(x), ncol=W)
+  for (i in 2:nrow(x)) {
+    for (j in 2:W) {
+      if (x[i, "w"] > j) {
+        mat[i, j] <- mat[i - 1, j]
+      }
+      else {
+        mat[i, j] <- max(mat[i - 1, j], mat[i - 1, j - x[i, "w"]] + x[i, "v"])
+      }
+    }
+  }
+  
+  return(mat)
 }
 
 greedy_knapsack <- function(x, W) {
